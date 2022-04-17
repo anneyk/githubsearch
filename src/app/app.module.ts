6a4e5e05fa793e290dade8523fb9e201cd9dd699
Annetkhavere <anneywan5@gmail.com>
2022-04-17 11:34:43 +0300

    add a new component know as the user-account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserAccountComponent } from './components/user-account/user-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAccountComponent
   ],
   imports: [
     BrowserModule,
